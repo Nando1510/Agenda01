f3edfef83d66958c7d963a11c25b1d0f37c47159
Nando1510 <117784292+Nando1510@users.noreply.github.com>
2025-09-01 21:39:21 -0300

    Create 15chama_funcao_CarregarDados_Automaticamente.js

---
+++ 15chama_funcao_CarregarDados_Automaticamente.js
@@ -0,0 +1,4 @@
+/* Essa linha garante que, assim que a página terminar de carregar o HTML, 
+sua função carregarDados() será chamada automaticamente para preencher a tabela. */
+
+window.addEventListener('DOMContentLoaded', carregarDados);
